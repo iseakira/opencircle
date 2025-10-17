3f9d88a1f5bae706d8b4a858390882ff540f79bf
kanikama999 <green927k@gmail.com>
2025-10-17 22:53:53 +0900

    refer to Yu's slack

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,53 @@
 // frontend/src/App.jsx
-
+import { useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
+
 function App() {
-  const [message, setMessage] = useState('サーバーからの返事を待っています...');
+  //const [message, setMessage] = useState('サーバーからの返事を待っています...');
 
-  useEffect(() => {
+  //useEffect(() => {
     // 1. この処理が呼ばれているか確認
-    console.log('useEffectが実行されました。これから通信を開始します。');
+    //console.log('useEffectが実行されました。これから通信を開始します。');
 
-    fetch('http://localhost:5001/api/hello')
-      .then((response) => {
+    //fetch('http://localhost:5001/api/hello')
+      //.then((response) => {
         // 2. サーバーからの「最初の返事」の中身を確認
-        console.log('サーバーからのレスポンス:', response);
-        if (!response.ok) {
-          throw new Error('ネットワークの応答がありませんでした');
-        }
-        return response.json();
-      })
-      .then((data) => {
+        //console.log('サーバーからのレスポンス:', response);
+        //if (!response.ok) {
+          //throw new Error('ネットワークの応答がありませんでした');
+        //}
+        //return response.json();
+      //})
+      //.then((data) => {
         // 3. JSONに変換されたデータの中身を確認
-        console.log('受け取ったデータ:', data);
-        setMessage(data.message);
-      })
-      .catch((error) => {
+        //console.log('受け取ったデータ:', data);
+        //setMessage(data.message);
+      //})
+      //.catch((error) => {
         // 4. もし何かエラーがあればここで表示
-        console.error('通信エラー:', error);
-        setMessage('バックエンドとの通信に失敗しました。');
-      });
-  }, []);
+        //console.error('通信エラー:', error);
+        //setMessage('バックエンドとの通信に失敗しました。');
+      //});
+  //}, []);
+
 
   return (
     <>
-      <h1>コンテナ間通信テスト</h1>
-      <p>{message}</p>
+      <div>
+        <h1>サークル情報</h1>
+        <p>サークルを掲載しています。</p>
+        <h2>サークル一覧</h2>
+
+
+      </div>
+
+      <footer>
+        <p>created by 東京理科大学IS科3年</p>
+        <a href="https://www.tus.ac.jp/" target="_blank">東京理科大学ホームページ</a>
+      </footer>
     </>
-  );
+  )
 }
 
 export default App;
