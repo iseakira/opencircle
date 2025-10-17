5c60add28e10f762c0c91ea6ac1d184129871db4
kanikama999 <green927k@gmail.com>
2025-10-18 00:37:44 +0900

    refer to Yu's slack

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@
 import { useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import './css/App.css' 
-
+import headImage from './images/head_image.png';
 
 function App() {
   //const [message, setMessage] = useState('サーバーからの返事を待っています...');
@@ -38,7 +38,7 @@ function App() {
     
       <body>
         <header class="page-header">
-          <h1><a href="images/head_images.png"></a></h1>
+          <h1><img class="logo" src={headImage} alt="アイコン" /></h1>
         </header>
         <h1>東京理科大学サークル情報</h1>
         <p>ここでは東京理科大学のサークル情報を掲載しています。</p>
