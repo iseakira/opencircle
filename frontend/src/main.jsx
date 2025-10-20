b62034d85885931094e49b4d91d50995c81f7107
IseAkira <akira.ise.m@gmail.com>
2025-10-20 16:54:34 +0900

    気にしないで

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,17 +2,15 @@ import { createRoot } from 'react-dom/client';
 import React from 'react';
 import App from './App.jsx';
 import Login from './Login.jsx';
-import { BrowserRouter,Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/Login" element={<Login />} />
       </Routes>
-
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
