68c76a6a0d24f86562d14c613a9287c953829245
kanikama999 <green927k@gmail.com>
2025-10-21 03:44:30 +0900

    タグ検索機能追加

--- frontend/src/pages/Toggle.jsx
+++ frontend/src/pages/Toggle.jsx
@@ -1,9 +1,58 @@
 import { useState } from 'react';
 
 function Toggle() {
+  //トグル操作
   const [visible, setVisible] = useState(false);
   const make_visible = () => {
     setVisible(!visible);
+  };
+  //検索エンジン
+  const [searchTerm, setSearchTerm] = useState('');
+  const change_searchTerm = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  //タグのステート
+  const [field, setField] = useState('');
+  const [circle_fee, setCircle_fee] = useState('');
+  const [gender_ratio, setGender_ratio] = useState('');
+  const [place, setPlace] = useState('');
+  const [mood, setMood] = useState('');
+  const [frequency, setFrequency] = useState('');
+  //タグ選択時の処理
+  const change_field =(e) =>{
+    setField(e.target.value);
+  };
+  const change_circle_fee =(e) =>{
+    setCircle_fee(e.target.value);
+  };
+  const change_gender_ratio =(e) =>{
+    setGender_ratio(e.target.value);
+  };
+  const change_place =(e) =>{
+    setPlace(e.target.value);
+  };
+  const change_mood =(e) =>{
+    setMood(e.target.value);
+  };
+  const change_frequency =(e) =>{
+    setFrequency(e.target.value);
+  };
+  //jsonデータ取得
+  const get_jsontags = () => {
+    const dataTosend = {
+      searchTerm: searchTerm,
+      field: field,
+      circle_fee: circle_fee,
+      gender_ratio: gender_ratio,
+      place: place,
+      mood: mood,
+      frequency: frequency
+    }
+    const json_stringdata = JSON.stringify(dataTosend);
+    console.log('タグのjsonデータ:', json_stringdata);
+    return json_stringdata;
+
   };
   return (
     <div>
@@ -12,13 +61,63 @@ function Toggle() {
       </button>
       {visible && (
         <div className="tag-select">
-          <form>
-            <input type="text" placeholder="検索" />
-          </form>
-          <h4>タグを選択してください</h4>
-          <div>
+          <h4>キーワードを入力するかタグを選択してください</h4>
+
+          
+          <div className="tags">
+            キーワード検索:<input type="text" placeholder="キーワードを入力" value={searchTerm} onChange={change_searchTerm} />
+            <br/>
             <label>分野</label>
+            <select value={field} onChange={change_field}>
+              <option value="">未選択</option>
+              <option value="運動">運動</option>
+              <option value="文化">文化</option>
+              <option value="音楽">音楽</option>
+              <option value="学生自治">学生自治</option>
+            </select>
+            <br/>
+            <label>費用</label>
+            <select value={circle_fee} onChange={change_circle_fee}>
+              <option value="">未選択</option>
+              <option value="0">無料</option>
+              <option value="2000円未満">2000円未満</option>
+              <option value="2000円以上">2000円以上</option>
+            </select>
+            <br/>
+            <label>男女比</label>
+            <select value={gender_ratio} onChange={change_gender_ratio}>
+              <option value="">未選択</option>
+              <option value="男性多め">男性多め</option>
+              <option value="女性多め">女性多め</option>
+              <option value="男女半々">男女半々</option>
+            </select>
+            <br/>
+            <label>活動場所</label>
+            <select value={place} onChange={change_place}>
+              <option value="">未選択</option>
+              <option value="学内">学内</option>
+              <option value="学外">学外</option>
+            </select>
+            <br/>
+            <label>雰囲気</label>
+            <select value={mood} onChange={change_mood}>
+              <option value="">未選択</option>
+              <option value="賑やか">落ち着いている</option>
+              <option value="energetic">エネルギッシュ</option>
+              <option value="focused">集中</option>
+            </select>
+            <br/>
+            <label>活動頻度</label>
+            <select value={frequency} onChange={change_frequency}>
+              <option value="">未選択</option>
+              <option value="週3未満">週3未満</option>
+              <option value="週3以上">週3以上</option>
+              <option value="不定期">不定期</option>
+            </select>
+            
           </div>
+          <br/>
+          <button onClick={get_jsontags}>絞り込む</button>
         </div>
       )}
     </div>
