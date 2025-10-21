e69963ce22665b0b61f4fb0aab565d7ce1fc55c5
Orz3838 <kumabara3wado@icloud.com>
2025-10-21 12:43:38 +0900

    requestをコメントアウトした

--- backend/app.py
+++ backend/app.py
@@ -18,8 +18,8 @@ def say_hello():
 
 @app.route('/hometest', methods=['POST'])
 def search():
-    joson_data = request.get_json()
-    
+    #joson_data = request.get_json()
+
     return jsonify({"message": "test"})
 
 # --- ここまでテスト用のコード ---
