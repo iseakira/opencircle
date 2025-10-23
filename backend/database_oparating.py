94ecca323d04e2fbdd738ef014b5f6b7c2de8ba0
Orz3838 <kumabara3wado@icloud.com>
2025-10-23 11:45:10 +0900

    WHERE句考え中

--- backend/database_oparating.py
+++ backend/database_oparating.py
@@ -14,4 +14,8 @@ def get_circle_search(json_dic):
     tmp_dict["place"]        = (cursor.execute("SELECT tag_id from Tag where tag_name = {}".format(json_dic["place"])        )).fetchone()[0]
     tmp_dict["mood"]         = (cursor.execute("SELECT tag_id from Tag where tag_name = {}".format(json_dic["mood"])         )).fetchone()[0]
     tmp_dict["frequency"]    = (cursor.execute("SELECT tag_id from Tag where tag_name = {}".format(json_dic["frequency"])    )).fetchone()[0]
-    res = cursor.execute("SELECT circle_name, circle_iconpath FROM Circle WHERE")
+    #TagとCircleの間の関係性の名前はどれだ？
+    res = cursor.execute("SELECT c.circle_name, c.circle_iconpath " \
+                        "FROM Circle AS c " \
+                        "JOIN circle_tag_table AS ctt ON c.circle_id = ctt.circle_id " \
+                        "WHERE ")#ここどうしようか考えてる
