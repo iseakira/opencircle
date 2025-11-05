d2c03e34cf30de87c28566b18639698261df22a9
Daiki77 <6323044@ed.tus.ac.jp>
2025-11-05 16:18:07 +0900

    kok

--- frontend/src/pages/CircleAdd.jsx
+++ frontend/src/pages/CircleAdd.jsx
@@ -163,7 +163,6 @@ function CircleAdd() {
     console.log(key, value);
   }
 
-<<<<<<< HEAD
   await sendData(formData);
     };
 
@@ -180,7 +179,7 @@ function CircleAdd() {
     // return json_stringdata;
 // console.log("送信データ内容",dataTosend);
 // await sendData(formData);
-  // };
+//   };
   // const sendData = async (json_stringdata) => {
   const sendData = async (formData) => {
     // const sendData = async (formData) => {
@@ -193,20 +192,6 @@ function CircleAdd() {
         // },
         // body: json_stringdata,
         credentials:"include",
-=======
-  };
-  const sendData = async (json_stringdata) => {
-
-
-    try {
-      const response = await fetch("http://localhost:5001/api/circles",{
-        method: "POST",
-        headers:{
-          'Content-Type': 'application/json',
-
-         },
-        body: json_stringdata,   
->>>>>>> b6f19307f90866cfc7298659e398e4a2696eba74
       });
 
 
