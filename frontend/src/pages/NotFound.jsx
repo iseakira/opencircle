3343fe0b0ca0db4289332c7a3ab3b46d46734354
Orz3838 <kumabara3wado@icloud.com>
2025-11-28 22:03:44 +0900

    サークルページへのリンクの変更と404ページの追加

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+import LoginOutButton from './LogInOutButton';
+import CircleLogo from '../conponents/CircleLogo';
+
+export function NotFound(){
+
+    return(
+        <>
+            <header className="page-header">
+                <CircleLogo></CircleLogo>
+                <h3>
+                    <LoginOutButton />
+                </h3>
+            </header>
+            <h1>
+                404エラーが出現したぞ
+            </h1>
+            <div>
+                非常に残念ながら、このページは存在していない、若しくは供養されました。<br />
+                <Link to="/">このリンク</Link>からホームに戻ることができます。<br />
+                ご不便おかけします。<br />
+            </div>
+        </>
+    )
+}
