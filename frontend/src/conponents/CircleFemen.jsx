d27cdcaad1922a32cde12efd032e1eecebca9009
kotomi18 <6323065@ed.tus.ac.jp>
2025-12-05 18:26:02 +0900

    修正

--- frontend/src/conponents/CircleFemen.jsx
+++ frontend/src/conponents/CircleFemen.jsx
@@ -4,13 +4,11 @@ export default function CircleFemen({value,onChange}) {
   return (
     <div style={{ marginBottom: "20px" }}>
       <label
-        htmlFor="Cmen"
+        htmlFor="Cfemen"
         style={{ display: "block" }} 
       >サークル人数（女性）</label>
-    <div>
       {/* <p>サークル人数（女性）</p> */}
       <input type="number" onChange={onChange} value={value} id="Cfemen"/>
-    </div>
     </div>
   )
 }
